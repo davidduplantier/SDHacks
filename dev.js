487e0a404499449ebd80725189a2f28696ec14f0
davidduplantier <dduplant@ucsd.edu>
2017-10-21 11:42:19 -0700

    Set up development scripts.

---
+++ dev.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const { Processor } = require('./processor')
+
+let processor = new Processor()
+
+processor.renderAll()
+
+fs.watch('views', { recursive: true }, (event, filename) => {
+  if ( path.extname(filename) === '.pug') {
+    processor.renderFile(filename)
+    console.log(`${filename} re-rendered`)
+  }
+})
+
+/*fs.watch('template.pug', (event, filename) => {
+  processor.renderAll()
+})*/
+
+const app = express()
+app.use( express.static('dist') )
+app.listen(3000)
